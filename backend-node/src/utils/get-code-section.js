db173644d8ec6ac80594b6403b6eca06033583d9
papitoulini <113286556+Papitoulini@users.noreply.github.com>
2024-11-06 19:14:05 +0200

    Enhance getCodeSection to include margin around code selection

--- backend-node/src/utils/get-code-section.js
+++ backend-node/src/utils/get-code-section.js
@@ -1,17 +1,30 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
 import fs from "fs/promises";
-// Get Code Section Operation
+
+const margin = 5; // Define the margin for extra lines
+
+// Get Code Section Operation with margin
 const getCodeSection = async (absolutePath, startLine, endLine) => {
 	try {
 		if (absolutePath) {
 			const data = await fs.readFile(absolutePath, "utf-8");
 			const lines = data.split(/\r?\n/);
-			const selectedLines = lines.slice(startLine - 1, endLine);
+
+			// Calculate start and end lines with margin
+			const adjustedStartLine = Math.max(0, startLine - margin - 1);
+			const adjustedEndLine = Math.min(lines.length, endLine + margin);
+
+			const selectedLines = lines.slice(adjustedStartLine, adjustedEndLine);
 			console.group("Retrieved Code Section:");
-			console.log("Start_line:", startLine, selectedLines[0]);
-			console.log("End_line :", endLine, selectedLines[selectedLines.length - 1]);
+			console.log("Start_line:", adjustedStartLine + 1, selectedLines[0]);
+			console.log("End_line :", adjustedEndLine, selectedLines[selectedLines.length - 1]);
 			console.groupEnd();
-			return selectedLines.join("\n");
+
+			return {
+				part: selectedLines.join("\n"),
+				startLine: adjustedStartLine + 1,
+				endLine: adjustedEndLine,
+			};
 		}
 
 		return null;
