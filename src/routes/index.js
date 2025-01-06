e736e65edc8c3432c467b33deb789b4dcc6ae823
papitoulini <113286556+Papitoulini@users.noreply.github.com>
2025-01-06 16:05:48 +0200

    remove unused files

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import eventRoutes from "./events.js";
-
-const router = express.Router({ mergeParams: true });
-
-router.use("/events/", eventRoutes);
-
-export default router;
