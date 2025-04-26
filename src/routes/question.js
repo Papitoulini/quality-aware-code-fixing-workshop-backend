37061b1b95789a7a3fd1c9c684b3f97d38e0669d
Papitoulini <panpapgeorgios@gmail.com>
2025-04-26 11:54:43 +0000

    add routs to fetch best responses

--- src/routes/question.js
+++ src/routes/question.js
@@ -7,7 +7,7 @@ import Sentry from "@sentry/node";
 import express from "express";
 import multer from "multer";
 
-import { analyzeFile } from "#utils";
+// import { analyzeFile } from "#utils";
 
 import { models } from "#dbs";
 
@@ -15,88 +15,88 @@ const { Question } = models;
 
 const uploadFolderPath = path.join(path.dirname(url.fileURLToPath(import.meta.url)), "..", "assets/uploads");
 
-const storage = multer.diskStorage({
-	destination: (req, _file, cb) => {
-		cb(null, uploadFolderPath);
-	},
-	filename: (req, file, cb) => {
-		req.body.originalName = file.originalname;
+// const storage = multer.diskStorage({
+// 	destination: (req, _file, cb) => {
+// 		cb(null, uploadFolderPath);
+// 	},
+// 	filename: (req, file, cb) => {
+// 		req.body.originalName = file.originalname;
 
-		let name = file.originalname;
-		name = name.replaceAll(/\s/g, ""); // Replace the special characters
+// 		let name = file.originalname;
+// 		name = name.replaceAll(/\s/g, ""); // Replace the special characters
 
-		const folder = Date.now().toString();
-		const saveName = `${folder}-${name}`;
+// 		const folder = Date.now().toString();
+// 		const saveName = `${folder}-${name}`;
 
-		req.body.saveName = saveName;
+// 		req.body.saveName = saveName;
 
-		// Create the folder with the project id if it does not exist
-		try {
-			fs.mkdirSync(path.join(uploadFolderPath, folder));
-		} catch { /* empty */ }
+// 		// Create the folder with the project id if it does not exist
+// 		try {
+// 			fs.mkdirSync(path.join(uploadFolderPath, folder));
+// 		} catch { /* empty */ }
 
-		// Pass folder to the request body
-		req.body.folder = folder;
+// 		// Pass folder to the request body
+// 		req.body.folder = folder;
 
-		cb(null, path.join(folder, saveName));
-	},
-});
+// 		cb(null, path.join(folder, saveName));
+// 	},
+// });
 
-const upload = multer({
-	storage,
-	fileFilter: (req, _, cb) => cb(null, true),
-}).fields([
-	{ name: "file", maxCount: 1 },
-]);
+// const upload = multer({
+// 	storage,
+// 	fileFilter: (req, _, cb) => cb(null, true),
+// }).fields([
+// 	{ name: "file", maxCount: 1 },
+// ]);
 
 const router = express.Router({ mergeParams: true });
 
-router.post("/", upload, async (req, res) => {
-	try {
-		const { folder, saveName, description, question } = req.body;
-		if (!saveName) {
-			return res.json({ success: false, message: "File Not Found" });
-		}
+// router.post("/", upload, async (req, res) => {
+// 	try {
+// 		const { folder, saveName, description, question } = req.body;
+// 		if (!saveName) {
+// 			return res.json({ success: false, message: "File Not Found" });
+// 		}
 
-		if (!description || !question) {
-			return res.json({ success: false, message: "Please Fill All Necessary Fields" });
-		}
+// 		if (!description || !question) {
+// 			return res.json({ success: false, message: "Please Fill All Necessary Fields" });
+// 		}
 
-		const analysisResults = await analyzeFile(path.join(uploadFolderPath, folder), saveName);
-		if (!analysisResults) {
-			return res.json({ success: false, message: "File Analysis Was Not Completed" });
-		}
+// 		const analysisResults = await analyzeFile(path.join(uploadFolderPath, folder), saveName);
+// 		if (!analysisResults) {
+// 			return res.json({ success: false, message: "File Analysis Was Not Completed" });
+// 		}
 
-		const code = fs.readFileSync(path.join(uploadFolderPath, folder, saveName), "utf8");
+// 		const code = fs.readFileSync(path.join(uploadFolderPath, folder, saveName), "utf8");
 
-		const analysis = {
-			sumOfSquaredQuality: analysisResults?.sumOfSquaredQuality || null,
-			harmonicMeanQuality: analysisResults?.harmonicMeanQuality || null,
-			...analysisResults?.scores,
-		};
+// 		const analysis = {
+// 			sumOfSquaredQuality: analysisResults?.sumOfSquaredQuality || null,
+// 			harmonicMeanQuality: analysisResults?.harmonicMeanQuality || null,
+// 			...analysisResults?.scores,
+// 		};
 
-		const maxIndex = await Question.findOne().sort({ index: -1 }).lean();
-		const index = maxIndex ? maxIndex.index + 1 : 1;
+// 		const maxIndex = await Question.findOne().sort({ index: -1 }).lean();
+// 		const index = maxIndex ? maxIndex.index + 1 : 1;
 
-		const newQuestion = new Question({
-			index,
-			code,
-			description,
-			question,
-			analysis,
-		});
-		await newQuestion.save();
+// 		const newQuestion = new Question({
+// 			index,
+// 			code,
+// 			description,
+// 			question,
+// 			analysis,
+// 		});
+// 		await newQuestion.save();
 
-		return res.json({
-			success: true,
-			message: "Query Was Not Found",
-		});
-	} catch (error) {
-		console.log(error);
-		Sentry.captureException(error);
-		return res.json({ message: "Something Went Wrong" });
-	}
-});
+// 		return res.json({
+// 			success: true,
+// 			message: "Query Was Not Found",
+// 		});
+// 	} catch (error) {
+// 		console.log(error);
+// 		Sentry.captureException(error);
+// 		return res.json({ message: "Something Went Wrong" });
+// 	}
+// });
 
 router.get("/:index", async (req, res) => {
 	try {
@@ -113,7 +113,6 @@ router.get("/:index", async (req, res) => {
 
 		return res.json({ success: true, question, hasNext: Boolean(nextQuestion) });
 	} catch (error) {
-		console.log(error);
 		Sentry.captureException(error);
 		return res.json({ success: false, message: "Something Went Wrong" });
 	}
