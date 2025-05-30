44b0dd834e6838d15dafbfb72384b9eb333748b7
Dimosthenis Natsos <natdimos@hotmail.com>
2025-05-30 15:34:55 +0300

    Fix parallel workers

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,15 @@ module.exports = {
 			name: "workshop-backend",
 			script: "npm",
 			args: "start",
+		},
+		{
+			name: "sast-worker",
+			script: "./src/workers/sast-worker.js",
+			instances: 3,
+			env: {
+				NODE_ENV: "production",
+				WORKER_INSTANCE: 0 // PM2 automatically sets this to 0,1,2 for each instance
+			}
 		}
 	],
 };
